2707bdb7f542d2c9b27fddb2eba9dfee3846f138
jaciporavieira@gmail.com <jaciporavieira@gmail.com>
2026-05-21 09:40:31 -0300

    docs: atualizando configurações do retrofit

--- app/src/main/java/com/example/bankapp/api/RetrofitClient.kt
+++ app/src/main/java/com/example/bankapp/api/RetrofitClient.kt
@@ -4,8 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    // Adicionado o context path '/api-android-helper/' que apareceu nos logs da sua API
-    private const val BASE_URL = "http://10.0.2.2:8080/api-android-helper/"
+        private const val BASE_URL = "http://10.0.2.2:8080/api-android-helper/"
 
     val instance: CryptoService by lazy {
         val retrofit = Retrofit.Builder()
